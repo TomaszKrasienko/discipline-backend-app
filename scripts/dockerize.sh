f011f4a2bdc5478889a5c960e7906eb27089da78
Tomasz Krasieńko <94125393+TomaszKrasienko@users.noreply.github.com>
2024-07-02 17:31:12 +0200

    Fixes

--- scripts/dockerize.sh
+++ scripts/dockerize.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+TAG=''
+VERSION_TAG=123
 REPOSITORY=tomaszkras/discipline-backend-app
 
+TAG='latest'
+VERSION_TAG=$TRAVIS_BUILD_NUMBER
+
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-docker build -t $REPOSITORY
+docker build f ./../discipline-backend-app/Dockerfile -t $REPOSITORY:$TAG -t $REPOSITORY:$VERSION_TAG .
+docker push $REPOSITORY:$VERSION_TAG
+docker push $REPOSITORY:$TAG
