2a55145374666b6350964ca7e7c3ad564678bd21
Tomasz Krasieńko <94125393+TomaszKrasienko@users.noreply.github.com>
2024-07-03 19:01:27 +0200

    Working on CI process

--- discipline-backed-app/dockerize.sh
+++ discipline-backed-app/dockerize.sh
@@ -1,12 +1,2 @@
 #!/bin/bash
-TAG=''
-VERSION_TAG=123
-REPOSITORY=tomaszkras/discipline-backend-app
-
-TAG='latest'
-# VERSION_TAG=$TRAVIS_BUILD_NUMBER
-
-docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-docker build -t $REPOSITORY:$TAG -t $REPOSITORY:$VERSION_TAG .
-docker push $REPOSITORY:$VERSION_TAG
-docker push $REPOSITORY:$TAG
+docker build -f ./discipline-backed-app/Dockerfile -t tomaszkras/discipline-backend-app .
