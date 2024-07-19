6352f40201e77b2f9f1e284d5a41c0e5e68fe6da
Tomasz Krasieńko <94125393+TomaszKrasienko@users.noreply.github.com>
2024-07-19 20:03:50 +0200

    Added user entity, value objects with create method and unit tests

---
+++ discipline-backend-app/src/discipline.application/Domain/Users/Entities/User.cs
@@ -0,0 +1,42 @@
+using discipline.application.Domain.SharedKernel;
+using discipline.application.Domain.Users.ValueObjects;
+
+namespace discipline.application.Domain.Users.Entities;
+
+internal class User
+{
+    public EntityId Id { get; }
+    public Email Email { get; private set; }
+    public Password Password { get; private set; }
+    public FullName FullName { get; private set; }
+
+    //For mongo
+    internal User(EntityId id, Email email, Password password, FullName fullName)
+    {
+        Id = id;
+        Email = email;
+        Password = password;
+        FullName = fullName;
+    }
+
+    private User(EntityId id)
+        => Id = id;
+
+    internal static User Create(Guid id, string email, string password, string firstName, string lastName)
+    {
+        var user = new User(id);
+        user.ChangeEmail(email);
+        user.ChangePassword(password);
+        user.ChangeFullName(firstName, lastName);
+        return user;
+    }
+
+    private void ChangeEmail(string email)
+        => Email = email;
+
+    private void ChangePassword(string password)
+        => Password = password;
+
+    private void ChangeFullName(string firstName, string lastName)
+        => FullName = new FullName(firstName, lastName);
+}
