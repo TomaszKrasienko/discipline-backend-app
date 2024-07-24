70a130c66584d1a82bf6cc478d234ac19ef6d949
Tomasz Krasieńko <94125393+TomaszKrasienko@users.noreply.github.com>
2024-07-24 22:05:45 +0200

    Added integration tests without subscription order  check

--- discipline-backend-app/tests/discipline.api.integration-tests/Users/CreateUserSubscriptionOrderTests.cs
+++ discipline-backend-app/tests/discipline.api.integration-tests/Users/CreateUserSubscriptionOrderTests.cs
@@ -3,6 +3,10 @@ using System.Net.Http.Json;
 using discipline.api.integration_tests._Helpers;
 using discipline.application.Domain.Users.Enums;
 using discipline.application.Features.Users;
+using discipline.application.Infrastructure.DAL.Documents.Users;
+using discipline.tests.shared.Documents;
+using discipline.tests.shared.Entities;
+using MongoDB.Driver;
 using Shouldly;
 using Xunit;
 
@@ -11,6 +15,27 @@ namespace discipline.api.integration_tests.Users;
 [Collection("integration-tests")]
 public sealed class CreateUserSubscriptionOrderTests : BaseTestsController
 {
+    [Fact]
+    public async Task CreateUserSubscriptionOrder_GivenExistingUserAndSubscription_ShouldReturn200OkStatusCodeAndAddToDb()
+    {
+        //arrange
+        var userDocument = UserDocumentFactory.Get();
+        var subscriptionDocument = SubscriptionDocumentFactory.Get();
+        await TestAppDb.GetCollection<UserDocument>().InsertOneAsync(userDocument);
+        await TestAppDb.GetCollection<SubscriptionDocument>().InsertOneAsync(subscriptionDocument);
+        var command = new CreateUserSubscriptionOrderCommand(Guid.Empty, Guid.Empty, subscriptionDocument.Id,
+            SubscriptionOrderFrequency.Monthly, new string('1', 15), "123");
+        
+        //act
+        var result = await HttpClient.PostAsJsonAsync($"/users/{userDocument.Id}/crate-subscription-order", command);
+        
+        //assert
+        result.StatusCode.ShouldBe(HttpStatusCode.OK);
+
+        var updatedUserDocument = await TestAppDb.GetCollection<UserDocument>().Find(x => x.Id == userDocument.Id)
+            .FirstOrDefaultAsync();
+    }
+    
     [Fact]
     public async Task CreateUserSubscriptionOrder_GivenNotExistingUser_ShouldReturn400BadRequestStatusCode()
     {
