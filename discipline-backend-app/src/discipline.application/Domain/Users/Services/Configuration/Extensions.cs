714edb7254065caac2b69cd80190f4fc05630412
Tomasz Krasieńko <94125393+TomaszKrasienko@users.noreply.github.com>
2024-07-27 13:39:16 +0200

    Improved CreateUserSubscriptionOrderTests

--- discipline-backend-app/src/discipline.application/Domain/Users/Services/Configuration/Extensions.cs
+++ discipline-backend-app/src/discipline.application/Domain/Users/Services/Configuration/Extensions.cs
@@ -6,5 +6,5 @@ namespace discipline.application.Domain.Users.Services.Configuration;
 internal static class Extensions
 {
     internal static IServiceCollection SetDomain(this IServiceCollection services)
-        => services.AddSingleton<ISubscriptionOrderService>();
+        => services.AddSingleton<ISubscriptionOrderService, SubscriptionOrderService>();
 }
--- discipline-backend-app/tests/discipline.api.integration-tests/Users/CreateUserSubscriptionOrderTests.cs
+++ discipline-backend-app/tests/discipline.api.integration-tests/Users/CreateUserSubscriptionOrderTests.cs
@@ -5,7 +5,6 @@ using discipline.application.Domain.Users.Enums;
 using discipline.application.Features.Users;
 using discipline.application.Infrastructure.DAL.Documents.Users;
 using discipline.tests.shared.Documents;
-using discipline.tests.shared.Entities;
 using MongoDB.Driver;
 using Shouldly;
 using Xunit;
@@ -16,11 +15,11 @@ namespace discipline.api.integration_tests.Users;
 public sealed class CreateUserSubscriptionOrderTests : BaseTestsController
 {
     [Fact]
-    public async Task CreateUserSubscriptionOrder_GivenExistingUserAndSubscription_ShouldReturn200OkStatusCodeAndAddToDb()
+    public async Task CreateUserSubscriptionOrder_GivenExistingUserAndPaidSubscription_ShouldReturn200OkStatusCodeAndAddToDb()
     {
         //arrange
         var userDocument = UserDocumentFactory.Get();
-        var subscriptionDocument = SubscriptionDocumentFactory.Get();
+        var subscriptionDocument = SubscriptionDocumentFactory.Get(10, 100);
         await TestAppDb.GetCollection<UserDocument>().InsertOneAsync(userDocument);
         await TestAppDb.GetCollection<SubscriptionDocument>().InsertOneAsync(subscriptionDocument);
         var command = new CreateUserSubscriptionOrderCommand(Guid.Empty, Guid.Empty, subscriptionDocument.Id,
@@ -34,6 +33,29 @@ public sealed class CreateUserSubscriptionOrderTests : BaseTestsController
 
         var updatedUserDocument = await TestAppDb.GetCollection<UserDocument>().Find(x => x.Id == userDocument.Id)
             .FirstOrDefaultAsync();
+        updatedUserDocument.SubscriptionOrder.ShouldBeOfType<PaidSubscriptionOrderDocument>();
+    }
+    
+    [Fact]
+    public async Task CreateUserSubscriptionOrder_GivenExistingUserAndFreeSubscription_ShouldReturn200OkStatusCodeAndAddToDb()
+    {
+        //arrange
+        var userDocument = UserDocumentFactory.Get();
+        var subscriptionDocument = SubscriptionDocumentFactory.Get();
+        await TestAppDb.GetCollection<UserDocument>().InsertOneAsync(userDocument);
+        await TestAppDb.GetCollection<SubscriptionDocument>().InsertOneAsync(subscriptionDocument);
+        var command = new CreateUserSubscriptionOrderCommand(Guid.Empty, Guid.Empty, subscriptionDocument.Id,
+            null, null, null);
+        
+        //act
+        var result = await HttpClient.PostAsJsonAsync($"/users/{userDocument.Id}/crate-subscription-order", command);
+        
+        //assert
+        result.StatusCode.ShouldBe(HttpStatusCode.OK);
+
+        var updatedUserDocument = await TestAppDb.GetCollection<UserDocument>().Find(x => x.Id == userDocument.Id)
+            .FirstOrDefaultAsync();
+        updatedUserDocument.SubscriptionOrder.ShouldBeOfType<FreeSubscriptionOrderDocument>();
     }
     
     [Fact]
@@ -51,11 +73,11 @@ public sealed class CreateUserSubscriptionOrderTests : BaseTestsController
     }
 
     [Fact]
-    public async Task CreateUserSubscriptionOrder_GivenTooShortCardNumber_ShouldReturn422UnprocessableEntityStatusCode()
+    public async Task CreateUserSubscriptionOrder_GivenEmptySubscriptionId_ShouldReturn422UnprocessableEntityStatusCode()
     {
         //arrange
-        var command = new CreateUserSubscriptionOrderCommand(Guid.Empty, Guid.Empty, Guid.NewGuid(),
-            SubscriptionOrderFrequency.Monthly, new string('1', 10), "123");
+        var command = new CreateUserSubscriptionOrderCommand(Guid.Empty, Guid.Empty, Guid.Empty,
+            SubscriptionOrderFrequency.Monthly, new string('1', 15), "123");
         
         //act
         var result = await HttpClient.PostAsJsonAsync($"/users/{Guid.NewGuid()}/crate-subscription-order", command);
