a2e18d003ae8f4f106b53725127009163fcae189
Tomasz Krasieńko <94125393+TomaszKrasienko@users.noreply.github.com>
2024-09-27 09:53:59 +0200

    Added unit tests for edit important date command handler

---
+++ discipline-backend-app/tests/discipline.application.unit-tests/Features/UsersCalendars/EditImportantDate/EditImportantDateCommandHandlerTests.cs
@@ -0,0 +1,65 @@
+using discipline.application.Behaviours;
+using discipline.application.Exceptions;
+using discipline.application.Features.UsersCalendars;
+using discipline.domain.Users.Entities;
+using discipline.domain.UsersCalendars.Entities;
+using discipline.domain.UsersCalendars.Repositories;
+using discipline.tests.shared.Entities;
+using NSubstitute;
+using Shouldly;
+using Xunit;
+
+namespace discipline.application.unit_tests.Features.UsersCalendars.EditImportantDate;
+
+public sealed class EditImportantDateCommandHandlerTests
+{
+    private Task Act(EditImportantDateCommand command) => _handler.HandleAsync(command, default);
+    
+    [Fact]
+    public async Task HandleAsync_GivenExistingUserCalendarForUserAndEventId_ShouldEditAndUpdateByRepository()
+    {
+        //arrange
+        var userCalendar = UserCalendarFactory.Get();
+        var eventId = Guid.NewGuid();
+        userCalendar.AddEvent(eventId, "test_title");
+        var command = new EditImportantDateCommand(Guid.NewGuid(), eventId, "new_test_title");
+
+        _userCalendarRepository
+            .GetByEventIdAsync(command.UserId, command.Id)
+            .Returns(userCalendar);
+        
+        //act
+        await Act(command);
+        
+        //assert
+        var @event = userCalendar.Events.First(x => x.Id.Value == command.Id);
+        @event.Title.Value.ShouldBe(command.Title);
+        
+        await _userCalendarRepository
+            .UpdateAsync(userCalendar);
+    }
+    
+    [Fact]
+    public async Task HandleAsync_GivenNotExistingUserCalendarForUserAndEventId_ShouldThrowUserCalendarNotFoundException()
+    {
+        //arrange
+        var command = new EditImportantDateCommand(Guid.NewGuid(), Guid.NewGuid(), "test_title");
+        
+        //act
+        var exception = await Record.ExceptionAsync(async () => await _handler.HandleAsync(command));
+        
+        //assert
+        exception.ShouldBeOfType<UserCalendarNotFoundException>();
+    }
+    
+    #region arrange
+    private readonly IUserCalendarRepository _userCalendarRepository;
+    private readonly ICommandHandler<EditImportantDateCommand> _handler;
+
+    public EditImportantDateCommandHandlerTests()
+    {
+        _userCalendarRepository = Substitute.For<IUserCalendarRepository>();
+        _handler = new EditImportantDateCommandHandler(_userCalendarRepository);
+    }
+    #endregion
+}
