849ae304b834b12381fa3afce2d52c66f23ef02f
Tomasz Krasieńko <94125393+TomaszKrasienko@users.noreply.github.com>
2024-11-03 12:27:55 +0100

    Added users infrastructure project with configuration

---
+++ discipline.centre/src/Modules/Users/discipline.centre.users.infrastructure/Configurations/UsersServicesInfrastructureConfigExtensions.cs
@@ -0,0 +1,9 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace discipline.centre.users.infrastructure.Configurations;
+
+public static class UsersServicesInfrastructureConfigExtensions
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        => services;
+}
