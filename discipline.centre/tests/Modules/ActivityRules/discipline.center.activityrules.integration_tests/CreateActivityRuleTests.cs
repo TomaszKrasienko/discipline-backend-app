64a6e59a342c74bedb1d15cb596077b36946a98a
Tomasz Krasieńko <94125393+TomaszKrasienko@users.noreply.github.com>
2024-11-22 21:59:10 +0100

    Added configuration, dependencies and empty create activity rules test file

---
+++ discipline.centre/tests/Modules/ActivityRules/discipline.center.activityrules.integration_tests/CreateActivityRuleTests.cs
@@ -0,0 +1,8 @@
+using discipline.centre.integration_tests.shared;
+
+namespace discipline.center.activityrules.integration_tests;
+
+public sealed class CreateActivityRuleTests() : BaseTestsController("activity-rules-module")
+{
+    
+}
