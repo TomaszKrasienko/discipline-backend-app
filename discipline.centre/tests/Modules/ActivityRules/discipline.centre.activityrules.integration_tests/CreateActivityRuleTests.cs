7261e734378d1439d672d9841508b0ac7f8b4887
Tomasz Krasieńko <94125393+TomaszKrasienko@users.noreply.github.com>
2024-11-22 22:04:08 +0100

    Changed name

--- discipline.centre/tests/Modules/ActivityRules/discipline.centre.activityrules.integration_tests/CreateActivityRuleTests.cs
+++ discipline.centre/tests/Modules/ActivityRules/discipline.centre.activityrules.integration_tests/CreateActivityRuleTests.cs
@@ -1,6 +1,6 @@
 using discipline.centre.integration_tests.shared;
 
-namespace discipline.center.activityrules.integration_tests;
+namespace discipline.centre.activityrules.integration_tests;
 
 public sealed class CreateActivityRuleTests() : BaseTestsController("activity-rules-module")
 {
