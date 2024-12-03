d756596261f678f81ee7bed9526ac8076ebb9882
Tomasz Krasieńko <94125393+TomaszKrasienko@users.noreply.github.com>
2024-12-03 22:37:45 +0100

    feat(ActivityRules): Added negative path for add stage

--- discipline.centre/tests/UnitTests/Modules/ActivityRules/discipline.centre.activityrules.domain.unit_tests/ActivityRulesTests/AddStageTests.cs
+++ discipline.centre/tests/UnitTests/Modules/ActivityRules/discipline.centre.activityrules.domain.unit_tests/ActivityRulesTests/AddStageTests.cs
@@ -1,6 +1,41 @@
+using discipline.centre.activityrules.domain.Specifications;
+using discipline.centre.activityrules.domain.ValueObjects.ActivityRules;
+using discipline.centre.shared.abstractions.SharedKernel.Exceptions;
+using discipline.centre.shared.abstractions.SharedKernel.TypeIdentifiers;
+using Shouldly;
+using Xunit;
+
 namespace discipline.centre.activityrules.domain.unit_tests.ActivityRulesTests;
 
 public sealed class AddStageTests
 {
+    [Fact]
+    public void ShouldThrowDomainExceptionWithCodeActivityRuleStagesMustHaveOrderedIndex_WhenStageHasInvalidIndex()
+    {
+        //arrange
+        var activityRule = ActivityRule.Create(ActivityRuleId.New(), UserId.New(), "test_title",
+            null, Mode.EveryDayMode, null, [new StageSpecification("test_stage1", 1)]);
+        
+        //act
+        var exception = Record.Exception(() => activityRule.AddStage(new StageSpecification("test_stage3", 3)));
+        
+        //assert
+        exception.ShouldBeOfType<DomainException>();
+        ((DomainException)exception).Code.ShouldBe("ActivityRule.Stages.MustHaveOrderedIndex");
+    }
     
+    [Fact]
+    public void ShouldThrowDomainExceptionWithCodeActivityRuleStagesStageTitleMustBeUnique_WhenStageHasNotUniqueTitle()
+    {
+        //arrange
+        var activityRule = ActivityRule.Create(ActivityRuleId.New(), UserId.New(), "test_title",
+            null, Mode.EveryDayMode, null, [new StageSpecification("test_stage1", 1)]);
+        
+        //act
+        var exception = Record.Exception(() => activityRule.AddStage(new StageSpecification("test_stage1", 2)));
+        
+        //assert
+        exception.ShouldBeOfType<DomainException>();
+        ((DomainException)exception).Code.ShouldBe("ActivityRule.Stages.StageTitleMustBeUnique");
+    }
 }
