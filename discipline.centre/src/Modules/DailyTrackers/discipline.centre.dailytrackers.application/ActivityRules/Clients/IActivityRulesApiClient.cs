3670bfc1e4355b45a52ddb1d8ef34bc01725ef8a
Tomasz Krasieńko <94125393+TomaszKrasienko@users.noreply.github.com>
2024-12-10 10:12:15 +0100

    feat(daily-trackers): added activity-rule client abstraction, moved activity rule and stage dto, added application unit tests project

--- discipline.centre/src/Modules/DailyTrackers/discipline.centre.dailytrackers.application/ActivityRules/Clients/IActivityRulesApiClient.cs
+++ discipline.centre/src/Modules/DailyTrackers/discipline.centre.dailytrackers.application/ActivityRules/Clients/IActivityRulesApiClient.cs
@@ -1,6 +1,8 @@
+using discipline.centre.shared.abstractions.SharedKernel.TypeIdentifiers;
+
 namespace discipline.centre.dailytrackers.application.ActivityRules.Clients;
 
 public interface IActivityRulesApiClient
 {
-    
+    Task<ActivityRuleId> GetActivityRuleByIdAsync(ActivityRuleId activityRuleId);
 }
