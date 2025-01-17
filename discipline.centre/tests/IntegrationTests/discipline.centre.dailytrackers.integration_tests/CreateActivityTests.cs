7685f504e40e198bd910ee19e6b716ce42139bc0
Tomasz Krasieńko <tomasz.krasienko@outlook.com>
2025-01-17 19:54:14 +0100

    Added integration tests

--- discipline.centre/tests/IntegrationTests/discipline.centre.dailytrackers.integration_tests/CreateActivityTests.cs
+++ discipline.centre/tests/IntegrationTests/discipline.centre.dailytrackers.integration_tests/CreateActivityTests.cs
@@ -1,6 +1,11 @@
+using System.Net;
+using System.Net.Http.Json;
 using discipline.centre.dailytrackers.application.DailyTrackers.DTOs;
 using discipline.centre.dailytrackers.domain.Specifications;
+using discipline.centre.dailytrackers.infrastructure.DAL.DailyTrackers.Documents;
 using discipline.centre.integration_tests.shared;
+using MongoDB.Driver;
+using Shouldly;
 using Xunit;
 
 namespace discipline.centre.dailytrackers.integration_tests;
@@ -11,13 +16,56 @@ public sealed class CreateActivityTests() : BaseTestsController("daily-trackers-
     public async Task GivenValidArguments_ShouldReturn200OkStatusCodeAndAddActivity()
     {
         //arrange 
-        var user = await AuthorizeWithoutSubscription();
+        var user = await AuthorizeWithFreeSubscriptionPicked();
 
         var createActivityDto = new CreateActivityDto(DateOnly.FromDateTime(DateTime.Now),
             new ActivityDetailsSpecification("test_activity_title", "test_activity_note"),
             [new StageSpecification("test_stage_title", 1)]);
         
         //act
-        var response = await HttpClient.PostAsync("api/daily-trackers-module/daily-trackers/activities")
+        var response = await HttpClient.PostAsJsonAsync("api/daily-trackers-module/daily-trackers/activities", createActivityDto);
+        
+        //assert
+        response.StatusCode.ShouldBe(HttpStatusCode.Created);
+
+        var identifier = GetResourceIdFromHeader(response);
+        
+        var isActivityExists = await TestAppDb.GetCollection<DailyTrackerDocument>()
+            .Find(x => x.Activities.Any(y => y.ActivityId.ToString() == identifier))
+            .AnyAsync();
+        
+        isActivityExists.ShouldBeTrue();
+    }
+    
+    [Fact]
+    public async Task GivenAlreadyExistingTitleForUserAndTheSameDay_ShouldReturn400BadRequest()
+    {
+        //arrange 
+        var user = await AuthorizeWithFreeSubscriptionPicked();
+
+        var createActivityDto = new CreateActivityDto(DateOnly.FromDateTime(DateTime.Now),
+            new ActivityDetailsSpecification("test_activity_title", "test_activity_note"), null);
+
+
+        var dailyTracker = new DailyTrackerDocument
+        {
+            DailyTrackerId = Ulid.NewUlid().ToString(),
+            Day = createActivityDto.Day,
+            UserId = user.Id.ToString(),
+            Activities = [new ActivityDocument
+            {
+                ActivityId = Ulid.NewUlid().ToString(),
+                IsChecked = false,
+                Title = createActivityDto.Details.Title,
+            }]
+        };
+        
+        await TestAppDb.GetCollection<DailyTrackerDocument>().InsertOneAsync(dailyTracker);
+        
+        //act
+        var response = await HttpClient.PostAsJsonAsync("api/daily-trackers-module/daily-trackers/activities", createActivityDto);
+        
+        //assert
+        response.StatusCode.ShouldBe(HttpStatusCode.BadRequest);
     }
 }
