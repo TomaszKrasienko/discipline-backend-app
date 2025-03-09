daa7604570a949bbdd83284621ecaa4ecf9f5d49
Tomasz Krasieńko <tomaszkrasienko@172.23.72.222.wi-fi.pjwstk.edu.pl>
2025-03-09 10:30:14 +0100

    feat(calendar): Added unit tests for create user calendar with time event

---
+++ discipline.centre/tests/Modules/Calendar/discipline.centre.calendar.domain.unit_tests/UserCalendarDayTests/CreateWithTimeEventTests.cs
@@ -0,0 +1,36 @@
+using discipline.centre.shared.abstractions.SharedKernel.TypeIdentifiers;
+using Shouldly;
+
+namespace discipline.centre.calendar.domain.unit_tests.UserCalendarDayTests;
+
+public class CreateWithTimeEventTests
+{
+    [Fact]
+    public void GivenValidArguments_WhenCreateWithTimeEvent_ThenShouldReturnUserCalendarDayWithTimeEvent()
+    {
+        // Arrange
+        var userCalendarId = UserCalendarId.New();
+        var userId = UserId.New();
+        var day = new DateOnly(2025, 1, 1);
+        var calendarEventId = CalendarEventId.New();
+        const string title = "Title";
+        const string description = "Description";
+        var from = new TimeOnly(10, 00);
+        var to = new TimeOnly(20, 00);
+        
+        // Act
+        var result = UserCalendarDay.CreateWithTimeEvent(userCalendarId, userId, day, calendarEventId, 
+            title, description, from, to);
+        
+        // Assert
+        result.Id.ShouldBe(userCalendarId);
+        result.UserId.ShouldBe(userId);
+        result.Day.Value.ShouldBe(day);
+        result.Events.Any(x 
+            => x is TimeEvent @event
+               && @event.Id == calendarEventId
+               && @event.Content is { Title: title, Description: description }
+               && @event.TimeSpan.From == from 
+               && @event.TimeSpan.To == to).ShouldBeTrue();
+    }
+}
